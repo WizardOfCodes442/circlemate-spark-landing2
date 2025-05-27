44d7ed0e3ef59114644e14c276042102ddb24875
josiah <adeagbojosiah1@gmail.com>
2025-05-27 11:27:59 +0100

    updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const { v4: uuidv4 } = require('uuid'); 
 const crypto = require('crypto');
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const path = require('path');
 const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { 
     authenticate, 
     verifyAdmin, 
