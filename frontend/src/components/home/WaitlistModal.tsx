bb7da02cc3eceae89a8642468e5e4d403b7c8f4f
josiah <adeagbojosiah1@gmail.com>
2025-05-31 21:10:14 +0100

    updated

--- frontend/src/components/home/WaitlistModal.tsx
+++ frontend/src/components/home/WaitlistModal.tsx
@@ -61,7 +61,7 @@ const handleSubmit = async (e: React.FormEvent) => {
   setIsSubmitting(true);
 
   try {
-    const response = await fetch("http://circlemate.loma.com.ng/waitlist/submit-waitlist.php", {
+    const response = await fetch("https://circlemate.loma.com.ng/waitlist/submit-waitlist.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
